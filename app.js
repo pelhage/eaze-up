2e15fc50dfbb2c0d5545f89e25435f9028d1353f
Patrick El-Hage <patrick.elhage@gmail.com>
2016-09-03 05:08:54 -0700

    Added ES6 support

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-require("babel-core").transform("code");
-
 // Dependencies
-var Nightmare = require('nightmare');
-var axios = require('axios');
-var moment = require('moment');
+import Nightmare from 'nightmare'
+import axios from 'axios'
+import moment from 'moment'
 
 // Instances
-var nightmare = Nightmare({ show: true })
+const nightmare = Nightmare({ show: true })
 
-var Strains = new RegExp(['Blue Dream', 'Sage'].join("|"), 'gi')
-
-var Clients = [
+const Clients = [
   {
     firstName: 'Patrick',
     phoneNumber: 4159365087,
-    strains: ['Blue Dream', 'Sage'],
+    desiredStrains: ['Blue Dream', 'Sage'],
   },
   // {
   //   name: 'Steve',
@@ -34,57 +30,74 @@ nightmare
   .click('place-group > place-item')
   // .type('form > input', '\u000d') // hit enter button
   .wait('.product h2')
-  .evaluate(function () {
-    return [].slice.call(document.querySelectorAll('.product')).map(function(el) {
+  .evaluate(() => {
+    return [].slice.call(document.querySelectorAll('.product')).map((el) => {
       return el.querySelector('h2').innerText
     })
   })
   .end()
-  .then(function(eazeStrains) {
-    console.log(eazeStrains);
-    var messageQueue = []
+  .then((eazeStrains) => {
+    console.log(eazeStrains)
+    const messageQueue = []
     // Check to see if each subscriber's strains are available
-    Clients.forEach(function(client) {
+    Stoners.forEach((stoner) => {
+      const { firstName, phoneNumber, desiredStrains } = stoner
       // Regex for the client's strains
-      var userStrains = new RegExp(client.strains.join("|"), 'gi')
-      var strainsFound = []
+      const userStrains = new RegExp(desiredStrains.join("|"), 'gi')
+      const strainsFound = []
       // Go through each strain on Eaze and see if there's a match
-      eazeStrains.forEach(function(eazeStrain) {
+      eazeStrains.forEach((eazeStrain) => {
         if (eazeStrain.match(userStrains)) {
           strainsFound.push(eazeStrain)
         }
       })
       // If the user's strains are available, add user to queue
       if (strainsFound.length) {
+        const textMessage = createTextMessage(firstName, strainsFound)
         messageQueue.push({
-          firstName: client.firstName,
-          phoneNumber: client.phoneNumber,
-          strains: strainsFound
+          firstName,
+          phoneNumber,
+          textMessage,
+          strainsFound
         })
       }
     })
     console.log('messageQueue', messageQueue)
     if (messageQueue.length) {
-      var date = moment().format('MM/DD/YY')
-      messageQueue.forEach(function(user) {
-        var message = '\nHi '+ user.firstName +
-        ', the following strains are available today (' +
-        date +') on eazeup.com: \n'
-        user.strains.forEach(function(strain) {
-          message += '\n' + strain
-        })
+      messageQueue.forEach(({ phoneNumber, textMessage }) => {
         axios.post('http://textbelt.com/text', {
-          'number': user.phoneNumber,
-          'message': message
-        }).then(function(response) {
-          console.log(response.data);
-        }).catch(function(error) {
-          console.log(error);
-        });
-
+          'number': phoneNumber,
+          'message': textMessage
+        }).then((response) => {
+          console.log(response.data)
+        }).catch((error) => {
+          console.log(error)
+        })
       })
     }
   })
-  .catch(function (error) {
-    console.error('Search failed:', error);
-  });
+  .catch((error) => {
+    console.error('Search failed:', error)
+  })
+
+
+
+/**
+ * createTextMessage - generates the text message string sent
+ * to users
+ *
+ * @param  {String} firstName       recipients first name
+ * @param  {Array} availableStrains requested strains available on eaze
+ * @return {String} message         message for recipient
+ */
+function createTextMessage(firstName, availableStrains) {
+  const dateTime = moment().format('MM/DD/YY')
+
+  let message = '\nHi ' + firstName +
+  ', the following strains are available today (' +
+  dateTime + ') on eazeup.com:\n'
+  availableStrains.forEach((strain) => {
+    message += '\n' + strain
+  })
+  return message
+}
