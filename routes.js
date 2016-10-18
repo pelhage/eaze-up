e1d77cb96d9acf80520c76b4277e37768c04d81c
Patrick El-Hage <patrick.elhage@gmail.com>
2016-10-17 20:49:40 -0700

    Fix chat bot bugs from refactor

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 import express from 'express'
 const Router = express.Router()
 import bodyParser from 'body-parser'
-import Patient from './lib/patient/patient-model'
+
+import chatMessage from './lib/sms-chat/chat-message'
+
 // Middleware
 Router.use(bodyParser.json())
 Router.use(bodyParser.urlencoded({ extended: true }))
@@ -13,31 +15,6 @@ Router.get('/', (req, res) => {
   res.send('Hello worlds')
 })
 
-Router.post('/message', (req, res) => {
-  // console.log('req', req.body);
-  let twiml = new TwimlResponse()
-  var patient = Patients
-
-  var patientMsg = req.body.Body.trim().split(" ")
-  var patientPhoneNumber = req.body.From
-
-  // console.log('patientCommand', patientCommand);
-  Patient.findOne({
-    'phoneNumber': patientPhoneNumber
-  }, function(err, existingPatient) {
-    if (err) { return next(err); }
-
-    var patient = existingPatient || new Patient({phoneNumber: patientPhoneNumber})
-
-    composeResponse(patientMsg, patient, twiml)
-
-    patient.save(function(err) {
-      if (err) { console.log(err) }
-    })
-    res.writeHead(200, {'Content-Type': 'text/xml'})
-    res.end(twiml.toString())
-
-  });
-})
+Router.post('/message', chatMessage)
 
 export default Router
