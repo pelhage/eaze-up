60f4fe393cdd89a069b4f090c483b911f1b52199
Patrick El-Hage <patrick.elhage@gmail.com>
2016-10-17 18:54:06 -0700

    Clean up import path

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ mongoose.connect(configDB.url); // connect to DB
 // Config API Routes
 import routes from './routes'
 app.use('/', routes)
-import Scraper from './lib/scraper/scraper'
+import Scraper from './lib/scraper'
+import Chron from './lib/chron'
+
 Scraper()
 // Listen Up!
 app.listen(3000, ()=>{ console.log('Listening on 3000')})
